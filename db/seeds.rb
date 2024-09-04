70165eaa7b155fe91819045cec7a7b72c853fe1e
guilermmm <guilherme.r.a.melo@gmail.com>
2024-09-04 15:07:59 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+15.times do
+  Categoria.create(
+    titulo: Faker::Book.genre
+  )
+end
+
+15.times do
+  Tag.create(
+    titulo: Faker::Books::Lovecraft.word
+  )
+end
+
+15.times do
+  f = File.open(Rails.root.join("app/assets/images/gato.jpg"))
+  # copy the file to another file in the same directory
+  FileUtils.cp(f.path, Rails.root.join("app/assets/images/gato_copy.jpg"))
+  Treinamento.create(
+    titulo: Faker::Books::Lovecraft.deity,
+    resumo: Faker::Books::Lovecraft.fhtagn,
+    corpo: Faker::Books::Lovecraft.paragraphs(number: 5).join("\n"),
+    data_publicacao: Faker::Date.between(from: 2.weeks.ago, to: Time.zone.today),
+    capa: File.new(Rails.root.join("app/assets/images/gato_copy.jpg")),
+    autor_id: SecureRandom.uuid,
+    categoria_id: Categoria.all.sample.id,
+    tags: [Tag.all.sample, Tag.all.sample]
+  )
+end
